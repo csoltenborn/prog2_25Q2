2beb3d8875549a2200eb4f51902a766ed73f6b20
Christian Soltenborn <christian.soltenborn@gmail.com>
2025-05-08 06:56:35 +0200

    Blatt 4 A1 b)

---
+++ src/main/java/battleship/model/Game.java
@@ -0,0 +1,44 @@
+package battleship.model;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Game {
+
+    private final List<Event> events;
+
+    public Game() {
+        this.events = new LinkedList<Event>();
+    }
+
+    public void addEvent(final Event event) {
+        this.events.add(event);
+    }
+
+    public Set<Coordinate> getActualShotCoordinates(final Player hitPlayer) {
+        return this.events.stream()
+            .filter(event -> event.isShotEvent(hitPlayer.inverse()))
+            .map(event -> ((Shot)event).coordinate)
+            .collect(Collectors.toSet());
+    }
+
+    public Stream<Event> getEvents() {
+        return this.events.stream();
+    }
+
+    public Stream<Event> getEventsByPlayer(final Player player) {
+        return this.events.stream().filter(event -> event.isShipPlacementEvent(player) || event.isShotEvent(player));
+    }
+
+    public Set<Coordinate> getShipCoordinates(final Player player) {
+        return this.events.stream()
+            .filter(event -> event.isShipPlacementEvent(player))
+            .flatMap(event -> ((ShipPlacement)event).toCoordinates())
+            .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+}
